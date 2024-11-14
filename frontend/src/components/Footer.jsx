5f252d0e78ad92200380a2c2d35e782f04fd40a9
uds822 <140148878+uds822@users.noreply.github.com>
2024-11-15 03:57:09 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -51,7 +51,7 @@ const Footer = () => {
           <div className="text-center md:text-start">
             <h2 className="text-lg font-semibold mb-4 text-white">Comparison</h2>
             <ul className="space-y-2">
-              {["DhiWise vs Anima", "DhiWise vs Appsmith", "DhiWise vs FlutterFlow", "DhiWise vs Monday Hero", "DhiWise vs Retool", "DhiWise vs Bubble", "DhiWise vs Figma Dev Mode"].map((item) => (
+              {["Aimal vs Anima"].map((item) => (
                 <li key={item}>
                   <a href="#" className="hover:text-blue-400">
                     {item}
@@ -95,10 +95,10 @@ const Footer = () => {
       <div className="bg-gray-800 py-4">
         <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
           <div className="text-xl font-semibold text-white md:flex hidden">
-            Cilli<span className="text-blue-400">Blog</span>
+            Your<span className="text-blue-400">Blog</span>
           </div>
           <p className="text-gray-400 text-sm md:order-none order-2 mt-4 md:mt-0">
-            &copy; 2024 DhiWise PVT. LTD. All rights reserved.
+            &All rights reserved.
           </p>
           <div className="flex space-x-6 text-gray-300 md:order-last order-1 mb-4 md:mb-0">
             <a href="#" aria-label="GitHub" className="hover:text-blue-400">
